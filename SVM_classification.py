c76e21e16dcb3ea2917ef5a920b7b17fbd7641f2
jorod18 <jorod18@student.sdu.dk>
2019-01-16 14:56:40 +0100

    add SVM classifier 1.o - working with polyps

--- SVM_classification.py
+++ SVM_classification.py
@@ -2,18 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
 
-classes = {'vegetation': 0, 'building': 1, 'water': 2}
+classes = {'wall': 0, 'polyp': 1, 'dirt': 2, 'dark': 3, 'hole': 4}
 n_classes = len(classes)
-palette = np.uint8([[0, 255, 0], [255, 0, 0], [0, 0, 255]])
-#blue, red, and green color, respectively.
-img = io.imread('https://i.stack.imgur.com/TFOv7.png')
+palette = np.uint8([[0, 255, 0], [255, 0, 0], [0, 0, 255], [0, 0, 0], [128,128,128]])
+
+img = io.imread('images/001.jpg')
+img2 = io.imread('images/001.jpg')
 rows, cols, bands = img.shape
 
 from sklearn.cluster import KMeans
 X = img.reshape(rows*cols, bands)
 kmeans = KMeans(n_clusters=n_classes, random_state=0).fit(X)
-unsupervised = kmeans.labels_.reshape(rows, cols)
-io.imshow(palette[unsupervised])
 
-plt.imshow(palette[unsupervised], cmap=plt.cm.binary)
+supervised = n_classes*np.ones(shape=(rows, cols), dtype=np.int)
+supervised[140:162, 29:50] = classes['polyp']
+supervised[20:35, 170:185] = classes['wall']
+#supervised[0:0, 0:0] = classes['dirt']
+supervised[0:10, 0:10] = classes['dark']
+supervised[165:180, 80:100] = classes['hole']
+
+y = supervised.ravel()
+train = np.flatnonzero(supervised < n_classes)
+test = np.flatnonzero(supervised == n_classes)
+
+from sklearn.svm import SVC
+clf = SVC()
+clf.fit(X[train], y[train])
+y[test] = clf.predict(X[test])
+supervised = y.reshape(rows, cols)
+#io.imshow(palette[supervised])
+plt.imshow(img2)
 plt.show()
+plt.imshow(palette[supervised], cmap=plt.cm.binary)
+plt.show()
+
+
